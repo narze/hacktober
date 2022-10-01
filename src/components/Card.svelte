e1187e526f3861cf2815943f38f19e9da109a75e
Manassarn Manoonchai <manassarn@gmail.com>
2022-10-01 15:46:12 +0000

    Add conditional rendering

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -12,24 +12,30 @@
   <Atropos innerClass="rounded-2xl">
     <div class="card image-full w-96 rounded-2xl bg-base-100 shadow-xl">
       <figure class="overflow-hidden">
-        <img src={imagePath} alt={title} width={500} fit="cover" />
+        {#if imagePath}
+          <img src={imagePath} alt={title} width={500} fit="cover" />
+        {/if}
       </figure>
       <div class="card-body">
         <h2 class="card-title">{title}</h2>
         <p>{description}</p>
         <div class="card-actions justify-end">
-          <a
-            class="btn btn-outline btn-sm normal-case"
-            href={webUrl}
-            target="_blank"
-            rel="noopener noreferrer">Website</a
-          >
-          <a
-            class="btn btn-outline btn-sm normal-case"
-            href={githubUrl}
-            target="_blank"
-            rel="noopener noreferrer">Code</a
-          >
+          {#if webUrl}
+            <a
+              class="btn btn-outline btn-sm normal-case"
+              href={webUrl}
+              target="_blank"
+              rel="noopener noreferrer">Website</a
+            >
+          {/if}
+          {#if githubUrl}
+            <a
+              class="btn btn-outline btn-sm normal-case"
+              href={githubUrl}
+              target="_blank"
+              rel="noopener noreferrer">Code</a
+            >
+          {/if}
         </div>
       </div>
     </div>
