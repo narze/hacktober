34b30d6fe081ea1d2bf1f880fde8cef844bc2ede
Intaniger <intaniger@gmail.com>
2022-10-16 14:59:11 +0700

    fix(gallery): :ambulance: fixed daisyui neutral variable

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -15,18 +15,19 @@
   export let githubUrl: string;
   export let imagePath: string | undefined;
 
-  const colorSet =
-    display == "light"
-      ? {
-          text: "text-HKBF-dark",
-          background: "display-light bg-HKBF-white",
-          button: "btn-secondary btn-active",
-        }
-      : {
-          text: "text-HKBF-white",
-          background: "bg-HKBF-dark",
-          button: "btn-primary btn-outline",
-        };
+  let colorSet = {
+    text: "text-HKBF-dark",
+    background: "display-light bg-HKBF-white",
+    button: "btn-secondary btn-active",
+  };
+
+  $: {
+    if (display === "dark") {
+      colorSet.text = "text-HKBF-white";
+      colorSet.background = "bg-HKBF-dark";
+      colorSet.button = "btn-primary btn-outline";
+    }
+  }
 </script>
 
 <div id="app" class="w-full">
