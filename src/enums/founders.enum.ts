f67ddf2c9fd767d73726007bd608bb3bbe0968d7
Siravijbb <siravijbb@gmail.com>
2022-10-22 21:29:40 +0700

    Added KNACKSAT2 TGPS Project

--- src/enums/founders.enum.ts
+++ src/enums/founders.enum.ts
@@ -1,5 +1,6 @@
 export enum Founders {
   Narze = "Manassarn Manoonchai",
   Intaniger = "Tanakorn Pisnupoomi",
-  Tinarskii = "Tinnaphat Somsang"
+  Tinarskii = "Tinnaphat Somsang",
+  KNACKSAT2TGPS = "KNACKSAT2TGPS Team"
 }
