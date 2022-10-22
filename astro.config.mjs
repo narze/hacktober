4aa6ff093a076561a9b4f841347919ae5b778999
ryanrw <surasak@opn.ooo>
2022-10-22 16:58:31 +0700

    feat(entries): add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), svelte()]
+  integrations: [tailwind(), image(), svelte(), mdx()]
 });
