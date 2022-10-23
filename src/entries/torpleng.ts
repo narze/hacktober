6a47b3ec37794da01743a7ce8aca769a5e8432ff
ryanrw <surasak@opn.ooo>
2022-10-23 12:21:26 +0700

    feat: change filetype from mdx to ts
    For Enum and nested object support

---
+++ src/entries/torpleng.ts
@@ -0,0 +1,12 @@
+import { Founders } from "../enums/founders.enum";
+import { AcademicTopics } from "../enums/topics.enum";
+
+export default {
+  display: "light",
+  title: "Torpleng - ต่อเพลง",
+  theme: "playful",
+  founderName: Founders.Narze,
+  topic: AcademicTopics.Vue,
+  imagePath: "/images/torpleng.jpg",
+  githubUrl: "https://github.com/narze/torpleng",
+};
