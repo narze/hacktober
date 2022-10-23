623964764b88d06d79da46159f8fcaa802d1f66f
Phoochit Witchutanon <phoochit1574@gmail.com>
2022-10-23 21:03:57 +0700

    format prettier

--- src/entries/tsuki.ts
+++ src/entries/tsuki.ts
@@ -7,6 +7,7 @@ export default {
   theme: "playful",
   founderName: Founders.Gusb3ll,
   topic: AcademicTopics.Python,
-  imagePath: "https://user-images.githubusercontent.com/77166960/154808873-1bdd3aab-1aa4-4fcd-a3e6-17dfcde3b720.png",
+  imagePath:
+    "https://user-images.githubusercontent.com/77166960/154808873-1bdd3aab-1aa4-4fcd-a3e6-17dfcde3b720.png",
   githubUrl: "https://github.com/phoowitchutanon/Tsuki",
 };
